5238247df58b27d948821e42cfd7b4f982829b3e
kishila <s16g459@stu.kagawa-u.ac.jp>
2017-03-18 22:15:55 +0900

    migrationの導入

---
+++ migrations/20170318102719-unnamed-migration.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable(
+    'user',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
